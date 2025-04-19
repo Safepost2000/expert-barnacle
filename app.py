76f57893bedc01a5833d08d93fce2736432c00be
Safepost2000 <safepost2000@gmail.com>
2025-04-19 16:05:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
-#!/usr/bin/python3
-import sqlite3
-
-def ConnectToDB(sdb):
-    return sqlite3.connect(sdb)
-
-print(sqlite3.sqlite_version)
-print(sqlite3.__path__)
-
-DB = ":memory:"
-con = ConnectToDB(DB)
-cur = con.cursor()
-cur.execute("SELECT sqlite_version(),sqlite_source_id();")
-for row in cur:
-     print(row[0] + '\r\n' + row[1])
-
-con.close()
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 import asyncio
 import logging
